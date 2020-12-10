9d48f822fbbf12b34aa3abc715b1d8c3c5ce2f98
Vinay <vinay1020287@gmail.com>
2020-12-10 00:57:09 -0500

    Remove unwanted files

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,7 +28,7 @@ export default class Header extends Component {
                                 {
                                 resumeData.socialLinks && resumeData.socialLinks.map(item =>{
                                 return(
-                                    <li key={item.name}>
+                                    <li key= {item.name}>
                                     <a href={item.url} target=""><i className={item.className}></i></a>
                                     </li>
                                 )
