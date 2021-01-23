f24e8e2c27d572222f8d61d60d290f83e759d13b
Vinay <vinay1020287@gmail.com>
2021-01-22 20:25:30 -0500

    Update contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 
 export default class Contact extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         name: '',
+    //         email: '',
+    //         subject: '',
+    //         message: ''
+    //     }
+    // }
+
     render() {
-        let resumeData = this.props.resumeData;
+        // let resumeData = this.props.resumeData;
         return (
             <section id="contact">
                 <div className="row section-head">
@@ -22,6 +33,10 @@ export default class Contact extends Component {
                                 <input id="email" className="input-text js-input" type="email" required/>
                             </div>
                             <div className="form-field col x-100">
+                                <label className="label" htmlFor="subject">Subject</label>
+                                <input id="subject" className="input-text js-input" type="text" required/>
+                            </div>
+                            <div className="form-field col x-100">
                                 <label className="label" htmlFor="message">Message</label>
                                 <input id="message" className="input-text js-input" type="text" required/>
                             </div>
@@ -32,30 +47,6 @@ export default class Contact extends Component {
                     </div>
                 </div>
             </section>
-            /*<section className="contact">
-                <div className="row section-head">
-                    <div className="ten columns">
-                        <h1 className="title">Get in touch</h1>
-                        <form className="contact-form row">
-                            <div className="form-field col x-50">
-                                <input id="name" className="input-text js-input" type="text" required/>
-                                <label className="label" htmlFor="name">Name</label>
-                            </div>
-                            <div className="form-field col x-50">
-                                <input id="email" className="input-text js-input" type="email" required/>
-                                <label className="label" htmlFor="email">E-mail</label>
-                            </div>
-                            <div className="form-field col x-100">
-                                <input id="message" className="input-text js-input" type="text" required/>
-                                <label className="label" htmlFor="message">Message</label>
-                            </div>
-                            <div className="form-field col x-100 align-center">
-                                <input className="submit-btn" type="submit" value="Submit"/>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </section>*/
         );
     }
 }
